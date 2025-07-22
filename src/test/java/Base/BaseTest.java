262a1a0149f3834ffe8125ee2487d6b3dba94ba2
oleks <oleksandr.klochenko@gmail.com>
2025-07-22 23:53:06 +0300

    222-07-2025 Add Allure report

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -2,7 +2,7 @@ package Base;
 
 import OLS.Pages.AuthPage.AuthPage;
 import OLS.Pages.BasePage.BasePageClass;
-import OLS.Pages.MainPage.MainPageClass;
+import OLS.Pages.MainPage.HomePageClass;
 import OLS.Pages.Step11Page.Step11PageClass;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.TestInstance;
@@ -17,7 +17,7 @@ public class BaseTest
     WebDriver driver=CommonActions.createDriver();
     public BasePageClass basePage=new BasePageClass(driver);
     public AuthPage authPage=new AuthPage(driver);
-    public MainPageClass mainPage=new MainPageClass(driver);
+    public HomePageClass homePage =new HomePageClass(driver);
     public Step11PageClass step11Page = new Step11PageClass(driver);
 
     @AfterAll
